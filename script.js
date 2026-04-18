8f59387ebf2bed08cb72caca9e5cc6313ea88868
Emmanuel Codez <emmacodez.dev@gmail.com>
2026-04-18 15:30:50 +0100

    navbar added

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// NAV BAR
+const headLinks = document.getElementById("headLinks");
+const toggle = document.getElementById("toggle");
+
+//
+// let toggleBtn = true;
+
+toggle.addEventListener("click", function () {
+  headLinks.classList.toggle("active");
+
+  // if (toggleBtn) {
+  //   // toggle.style.backgroundColor = "blue";
+  //   toggle.textContent = "❌";
+  //   toggle.style.fontSize = "30px";
+  // } else {
+  //   // toggle.style.backgroundColor = "green";
+  //   toggle.textContent = "☰";
+  // }
+
+  // toggleBtn = !toggleBtn;
+});
+
+// AOS HEADER
+
 const popup = document.querySelector(".pop-up-cont");
 const hero = document.querySelector(".My-Hero");
 
